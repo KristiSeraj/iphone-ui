0a3d264e704898755b947597d3300a9215f6d723
MrCoku <65773022+MrCoku@users.noreply.github.com>
2021-07-13 23:33:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('textClock').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    };
+    return i;
+}
