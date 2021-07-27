81bb8215ca819b64c93e21c11e6b5240a344a4e5
MrCoku <65773022+ElProfesor-dev@users.noreply.github.com>
2021-07-27 18:18:08 +0200

    clock and time fixed in js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+"use strict";
+
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    m = checkTime(m);
-    document.getElementById('textClock').innerHTML = h + ":" + m;
-    setTimeout(startTime, 1000);
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById('textClock').innerHTML = h + ":" + m;
+  document.getElementById('fullTime').innerHTML = dayNames[today.getDay()] + ", " + monthNames[today.getMonth()] + " " + today.getDate();
+  setTimeout(startTime, 1000);
 }
 
 function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
-    };
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+
+  ;
+  return i;
 }
-
-
-//working on time
