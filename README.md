7fb1c64072eb7f3bcc02ca3d1a4f81ed2fb00650
MrCoku <65773022+ElProfesor-dev@users.noreply.github.com>
2021-08-02 17:13:09 +0200

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# IPHONE UI
+
+IPHONE created using HTML, CSS And JavaScript.
+
+## IMAGES
+
+![](/src/asset/src.png)
+
+## Contributing
+
+Feel free to contribute to this project.
+
+## SUPPORT
+
+<a href="https://www.buymeacoffee.com/mrmothdevs" target="_blank"><img src="https://img.shields.io/badge/Buy_Me_A_Coffee-FFDD00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black" /></a>
---
+++ src/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById('textClock').innerHTML = h + ":" + m;
+  document.getElementById('fullTime').innerHTML = dayNames[today.getDay()] + ", " + monthNames[today.getMonth()] + " " + today.getDate();
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  };
+  return i;
+}
